a00faeb9cc3e4c8ea9d5f0daab9652d42aacdd5e
gon <'gonsland99@gmail.com'>
2022-02-17 23:01:12 +0900

    script를 이용한 로그인 시 유효성검사

---
+++ jspStudy/src/main/java/ch6/LoginServlet3.java
@@ -0,0 +1,31 @@
+package ch6;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+    public LoginServlet3() {
+    	System.out.println("init");
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		System.out.println(id);
+		System.out.println(pw);
+		
+		
+	}
+
+}
