2f61b0ecde6b5fafd24728622c1976e4d87aa0fe
gon <'gonsland99@gmail.com'>
2022-03-20 01:17:40 +0900

    aop lombok 문제?

--- jspStudy/src/main/java/ch20/LoggingAdvice.java
+++ jspStudy/src/main/java/ch20/LoggingAdvice.java
@@ -5,13 +5,13 @@ import org.aopalliance.intercept.MethodInvocation;
 
 public class LoggingAdvice implements MethodInterceptor {
 	public Object invoke(MethodInvocation invocation) throws Throwable {
-		System.out.println("method 호출 전: LogginAdvice");
-		System.out.println(invocation.getMethod() + "method 호출 전");
+		System.out.println("전 로깅 : LogginAdvice");
+		System.out.println(invocation.getMethod() + "전 로깅");
 
 		Object object = invocation.proceed();
 
-		System.out.println("method 호출 후: loggingAdvice");
-		System.out.println(invocation.getMethod() + "method 호출 후");
+		System.out.println("후 로깅: loggingAdvice");
+		System.out.println(invocation.getMethod() + "후 로깅");
 		return object;
 	}
 }
