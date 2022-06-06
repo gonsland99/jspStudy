7ea5f57486ac1e3a78bd861968aeea42723db982
gon <'gonsland99@gmail.com'>
2022-06-06 18:54:29 +0900

    dispatcher 포워딩

---
+++ jspStudy/src/main/java/pro08/sec03/ex01/SecondServlet.java
@@ -0,0 +1,24 @@
+package pro08.sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.print("<html><body>");
+		out.print(name+"<br/>");
+		out.print("dispatch 포워드 실습");
+		out.print("</body></html>");
+	}
+}
