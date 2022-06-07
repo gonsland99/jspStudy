ce3c6773636de36f017d2de76670a8f29389fc22
gon <'gonsland99@gmail.com'>
2022-06-07 22:01:02 +0900

    서블릿 속성별 스코프

---
+++ jspStudy/src/main/java/pro10/sec01/ex01/SetAttribute.java
@@ -0,0 +1,32 @@
+package pro10.sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+//@WebServlet("/set")
+public class SetAttribute extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String ctxMesg = "context에 바인딩";	
+		String sesMesg = "session에 바인딩";	
+		String reqMesg = "request에 바인딩";
+		
+		ServletContext ctx = getServletContext();
+		HttpSession session = request.getSession();
+		ctx.setAttribute("context", ctxMesg);
+		session.setAttribute("session", sesMesg);
+		request.setAttribute("request", reqMesg);
+		out.print("바인딩 수행");
+	}
+}
