934fb7bcb1d275158279c204ee17262dc8d1bf50
gon <'gonsland99@gmail.com'>
2022-06-15 11:09:47 +0900

    게시판 글목록 구현

--- jspStudy/src/main/java/pro17/sec02/ex01/BoardController.java
+++ jspStudy/src/main/java/pro17/sec02/ex01/BoardController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/board/*")
+//@WebServlet("/board/*")
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     BoardService service;
