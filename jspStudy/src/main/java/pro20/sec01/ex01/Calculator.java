e889d45ded31d5f654a2ff1116ab55e1d39851b7
gon <'gonsland99@gmail.com'>
2022-06-19 21:33:03 +0900

    aop test(재확인)

--- jspStudy/src/main/java/pro20/sec01/ex01/Calculator.java
+++ jspStudy/src/main/java/pro20/sec01/ex01/Calculator.java
@@ -2,19 +2,22 @@ package pro20.sec01.ex01;
 
 public class Calculator {
 	public void add(int x, int y) {
-		int result = x+y;
-		System.out.println("결과: "+result);
+		int result=x+y;
+		System.out.println("°á°ú:"+ result);
 	}
+
 	public void subtract(int x, int y) {
-		int result = x-y;
-		System.out.println("결과: "+result);
+		int result=x - y;
+		System.out.println("°á°ú:"+ result);
 	}
+
 	public void multiply(int x, int y) {
-		int result = x*y;
-		System.out.println("결과: "+result);
+		int result=x * y;
+		System.out.println("°á°ú:"+ result);
 	}
+
 	public void divide(int x, int y) {
-		int result = x/y;
-		System.out.println("결과: "+result);
+		int result=x / y;
+		System.out.println("°á°ú:"+ result);
 	}
 }
--- jspStudy/src/main/java/pro20/sec01/ex01/LoggingAdvice.java
+++ jspStudy/src/main/java/pro20/sec01/ex01/LoggingAdvice.java
@@ -3,16 +3,15 @@ package pro20.sec01.ex01;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
-public class LoggingAdvice implements MethodInterceptor{
-	public Object invoke(MethodInvocation invocation) throws Throwable{
-		System.out.println("메서드 호출 전");
-		System.out.println(invocation.getMethod());
-		
+public class LoggingAdvice implements MethodInterceptor {
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		System.out.println("[메서드 호출 전 : LogginAdvice");
+		System.out.println(invocation.getMethod() + "메서드 호출 전");
+
 		Object object = invocation.proceed();
-		
-		System.out.println("메서드 호출 후");
-		System.out.println(invocation.getMethod());
-		
+
+		System.out.println("[메서드 호출 후 : loggingAdvice");
+		System.out.println(invocation.getMethod() + "메서드 호출 후");
 		return object;
 	}
 }
